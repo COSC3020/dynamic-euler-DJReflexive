9e4777838d590e4e6f0c4c3f05d1c50c228e9f3b
DJReflexive <89596189+DJReflexive@users.noreply.github.com>
2024-11-08 10:26:17 -0700

    Submission 2. Updates to code.js & README

--- code.js
+++ code.js
@@ -14,12 +14,14 @@ function factorial(n) {
 
 function e(n) {
     let result = 1.0;
+    let fac = 1.0;
 
     if (n < 0) { return null; } // Negative Numbers Aren't Allowed
     if (n == 0) { return result; }
 
     for (let i = 1; i <= n; i++) {
-        result +=  (1.0 / factorial(i));
+        fac *= i;
+        result +=  (1.0 / fac);
     }
 
     return result;
